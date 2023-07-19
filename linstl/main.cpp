62b02dd64223176d48639ba7e81bf035bac5b918
xiaolin1111 <135115678+xiaolin1111@users.noreply.github.com>
2023-07-19 10:06:03 +0800

    Update main.cpp

--- linstl/main.cpp
+++ linstl/main.cpp
@@ -1,18 +1,1 @@
-#include "vector.h"
-#include <iostream>
-using namespace linstl;
-int main()
-{
-    vector<int> vec;
-    
-    for(int i = 0;i<5;i++)
-    {
-        vec.push_back(i);
-    }
-    for(int i = 0;i<5;i++)
-    {
 
-        std::cout<<vec[i]<<" "<<std::endl;
-    }
-    return 0;
-}
